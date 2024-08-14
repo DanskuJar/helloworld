3255b2eeea8cb8c2da4879e07005f785fc4889ec
DanskuJar <dansku.jarvinen@gmail.com>
2024-08-14 14:36:47 +0300

    if and else

--- app.ts
+++ app.ts
@@ -1,4 +1,9 @@
 let message: string = 'Foo bar';
-let thisnumber: number = 6;
+let thisnumber: number = 4;
 console.log(message.toLowerCase());
 console.log(thisnumber);
+if (thisnumber < 5) {
+    console.log(message);
+} else {
+    console.log(thisnumber);
+}
