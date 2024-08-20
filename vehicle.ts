adb3c5f141ea8f020a1628072936c559ca717b75
DanskuJar <dansku.jarvinen@gmail.com>
2024-08-20 20:26:15 +0300

    motorbike

--- vehicle.ts
+++ vehicle.ts
@@ -1,5 +1,8 @@
-// Luokka Vehicle tähän
-
-// Tee tarvittavat muutokset alla oleviin riveihin
-const vehicle = "Motorbike";
-console.log(vehicle);
+class Vehicle {
+    brand: string;
+    numWheels: number;
+}
+const vehicle = new Vehicle();
+vehicle.brand = "Kawasaki";
+vehicle.numWheels = 2;
+console.log(vehicle.numWheels);
