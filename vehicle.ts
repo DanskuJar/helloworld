00a5edae668a0881b32896a94cc092ac6af3a7c0
Timo Järvinen <timo.jarvinen@yahoo.com>
2024-08-27 10:24:00 +0300

    Add constructor to class MotorBike

--- vehicle.ts
+++ vehicle.ts
@@ -25,13 +25,18 @@ class Vehicle implements Maintenance {
     }
 }
 
-class motorBike extends Vehicle {
+class MotorBike extends Vehicle {
     
     private cc: number = 5;
+
+    public constructor(protected readonly b: string, protected readonly n: number, protected readonly c: number) {
+        super(b, n);
+        this.cc = c;
+      }
     
     public getCc(): number {
         return this.cc;
     }
 }
-const vehicle = new motorBike("motorBike", 2);
+const vehicle = new MotorBike("motorBike", 2, 125);
 console.log(vehicle.getCc());
